90f8a24f4543199b4bc5d705bc4a3621692128d5
Michael Monson <michaelmonson@ldschurch.org>
2014-12-15 10:11:47 -0700

    Updating basic parenthesis support

--- MichaelMonson/src/ExpressionSolve.java
+++ MichaelMonson/src/ExpressionSolve.java
@@ -24,102 +24,126 @@ public class ExpressionSolve {
         	//Clear all whitespace:
             mathProblem = mitigateWhiteSpace(mathProblem);
             System.out.println("\n\nOriginal Problem: \n" + mathProblem);
-        	
-        	// Check for parenthesis:
-            // This should be the easiest part, since I just take what's inside the left and right paren's
-        	
+
+                    	
         	//Evaluate using Arithmetic Rules:
         	int leftNumber = 0;
         	int rightNumber = 0;
 
         	
-        	//Multiply and Divide:
-        	while (solution == 0) {
-	        	int multiplyIndex = mathProblem.indexOf("*");
-	        	int divideIndex = mathProblem.indexOf("/");
-	        	int operatorIndex = 0;
-	        	
-	    		if (multiplyIndex > 0 || divideIndex > 0) {
-	    			
-	    			int calculate = 0;
-	    			
-	    			if (multiplyIndex > 0 && (divideIndex <= 0 || divideIndex > multiplyIndex)) {
-	    				leftNumber = constructLeftNumber(mathProblem, multiplyIndex);
-	    				rightNumber = constructRightNumber(mathProblem, multiplyIndex);
-	    				operatorIndex = multiplyIndex;
-	    				calculate = leftNumber * rightNumber;
-	    				
-	    			} else if (divideIndex > 0 && (multiplyIndex <= 0 || multiplyIndex > divideIndex)) {
-	    				leftNumber = constructLeftNumber(mathProblem, divideIndex);
-	    				rightNumber = constructRightNumber(mathProblem, divideIndex);
-	    				operatorIndex = divideIndex;
-	    				calculate = leftNumber / rightNumber;
-	    			}
-	    			
-	    			//Display Calculation and merge back:
-	    			System.out.println("   -->: " + leftNumber + mathProblem.charAt(operatorIndex) + rightNumber + " = " + calculate);
-	    			
-	    			mathProblem = mathProblem.substring(0, operatorIndex - String.valueOf(leftNumber).length())
-	    					+ String.valueOf(calculate)
-	    					+ mathProblem.substring(operatorIndex + String.valueOf(rightNumber).length() + 1, mathProblem.length());
-	    			
-	    			System.out.println("\n" + mathProblem);
-	    			
-	    		} else {
-	    			break;
-	    		}
+        	/* Check for parenthesis:
+        	 *   TODO:  Expand support from a single set of paren's to evaluate multiple and nested parenthesis sets:
+        	 */        		
+        	int leftParen = mathProblem.indexOf("(");
+        	int rightParen = mathProblem.indexOf(")");
+        	
+        	if (leftParen != -1) {
+        		String subFormula = mathProblem.substring(leftParen + 1, rightParen);        		
+        		String answer = evaluateStatement(subFormula);
+        		mathProblem = mathProblem.substring(0, leftParen)
+    					+ String.valueOf(answer)
+    					+ mathProblem.substring(rightParen + 1, mathProblem.length());
         	}
         	
-        	
-        	//Addition and Subtraction:
-        	while (solution == 0) {
-	        	int addIndex = mathProblem.indexOf("+");
-	        	int subtractIndex = mathProblem.indexOf("-");
-	        	int operatorIndex = 0;
-	        	
-	    		if (addIndex > 0 || subtractIndex > 0) {
-	    			
-	    			int calculate = 0;
-	    			
-	    			if (addIndex > 0 && (subtractIndex <= 0 || subtractIndex > addIndex)) {
-	    				leftNumber = constructLeftNumber(mathProblem, addIndex);
-	    				rightNumber = constructRightNumber(mathProblem, addIndex);
-	    				operatorIndex = addIndex;
-	    				calculate = leftNumber + rightNumber;
-	    				
-	    			} else if (subtractIndex > 0 && (addIndex <= 0 || addIndex > subtractIndex)) {
-	    				leftNumber = constructLeftNumber(mathProblem, subtractIndex);
-	    				rightNumber = constructRightNumber(mathProblem, subtractIndex);
-	    				operatorIndex = subtractIndex;
-	    				calculate = leftNumber - rightNumber;
-	    			}
-	    			
-	    			//Display Calculation and merge back:
-	    			System.out.println("   -->: " + leftNumber + mathProblem.charAt(operatorIndex) + rightNumber + " = " + calculate);
-	    			
-	    			mathProblem = mathProblem.substring(0, operatorIndex - String.valueOf(leftNumber).length())
-	    					+ String.valueOf(calculate)
-	    					+ mathProblem.substring(operatorIndex + String.valueOf(rightNumber).length() + 1, mathProblem.length());
-	    			
-	    			System.out.println("\n" + mathProblem);
-	    			
-	    		} else {
-	    			break;
-	    		}
-	    		
-        	}
-        		
+        	//Parenthesis cleared.  Now evaluate the rest of the problem:
+        	String finalAnswer = evaluateStatement(mathProblem);
     		
-    		System.out.println("\n\n FINAL ANSWER: " + mathProblem);
+    		System.out.println("\n\n FINAL ANSWER: " + finalAnswer);
 
 
         } catch (Exception e) {
-        	System.out.println( "\n  An error has occured..." );
+        	System.out.println( "\n  An error has occured... invalid characters may have been entered" );
         }
-		
         
+	}
+	
+	
+	private static String evaluateStatement (String statement) {
+		
+    	int leftNumber = 0;
+    	int rightNumber = 0;
+    	int solution = 0;
+		
+    	//Multiplication and Division:
+    	while (solution == 0) {
+        	int multiplyIndex = statement.indexOf("*");
+        	int divideIndex = statement.indexOf("/");
+        	int operatorIndex = 0;
+        	
+    		if (multiplyIndex > 0 || divideIndex > 0) {
+    			
+    			int calculate = 0;
+    			
+    			if (multiplyIndex > 0 && (divideIndex <= 0 || divideIndex > multiplyIndex)) {
+    				leftNumber = constructLeftNumber(statement, multiplyIndex);
+    				rightNumber = constructRightNumber(statement, multiplyIndex);
+    				operatorIndex = multiplyIndex;
+    				calculate = leftNumber * rightNumber;
+    				
+    			} else if (divideIndex > 0 && (multiplyIndex <= 0 || multiplyIndex > divideIndex)) {
+    				leftNumber = constructLeftNumber(statement, divideIndex);
+    				rightNumber = constructRightNumber(statement, divideIndex);
+    				operatorIndex = divideIndex;
+    				calculate = leftNumber / rightNumber;
+    			}
+    			
+    			//Display Calculation and merge back:
+    			System.out.println("   -->: " + leftNumber + statement.charAt(operatorIndex) + rightNumber + " = " + calculate);
+    			
+    			statement = statement.substring(0, operatorIndex - String.valueOf(leftNumber).length())
+    					+ String.valueOf(calculate)
+    					+ statement.substring(operatorIndex + String.valueOf(rightNumber).length() + 1, statement.length());
+    			
+    			System.out.println("\n" + statement);
+    			
+    		} else {
+    			break;
+    		}
+    	}
+    	
+    	
+    	//Addition and Subtraction:
+    	while (solution == 0) {
+        	int addIndex = statement.indexOf("+");
+        	int subtractIndex = statement.indexOf("-");
+        	int operatorIndex = 0;
+        	
+    		if (addIndex > 0 || subtractIndex > 0) {
+    			
+    			int calculate = 0;
+    			
+    			if (addIndex > 0 && (subtractIndex <= 0 || subtractIndex > addIndex)) {
+    				leftNumber = constructLeftNumber(statement, addIndex);
+    				rightNumber = constructRightNumber(statement, addIndex);
+    				operatorIndex = addIndex;
+    				calculate = leftNumber + rightNumber;
+    				
+    			} else if (subtractIndex > 0 && (addIndex <= 0 || addIndex > subtractIndex)) {
+    				leftNumber = constructLeftNumber(statement, subtractIndex);
+    				rightNumber = constructRightNumber(statement, subtractIndex);
+    				operatorIndex = subtractIndex;
+    				calculate = leftNumber - rightNumber;
+    			}
+    			
+    			//Display Calculation and merge back:
+    			System.out.println("   -->: " + leftNumber + statement.charAt(operatorIndex) + rightNumber + " = " + calculate);
+    			
+    			statement = statement.substring(0, operatorIndex - String.valueOf(leftNumber).length())
+    					+ String.valueOf(calculate)
+    					+ statement.substring(operatorIndex + String.valueOf(rightNumber).length() + 1, statement.length());
+    			
+    			System.out.println("\n" + statement);
+    			
+    		} else {
+    			break;
+    		}
+    		
+    	}
+		
+    	return statement;
 		
 	}
+	
 	
 	private static int constructLeftNumber (String currentMathProblem, int index) {
 		
